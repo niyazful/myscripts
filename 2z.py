dd8cc5327dfa38179b23423281259415cb3a29ce
niyazful <56266317+niyazful@users.noreply.github.com>
2019-10-08 12:05:19 +0500

    Delete 2z.py

--- 2z.py
+++
@@ -1,9 +0,0 @@
-import json
-
-mayfile = "task_2.json"
-with open(mayfile,encoding="utf-8") as read_file:
-    data = json.load(read_file)
-    m
-    y
-    data.sort(key=lambda user: user["shop"])
-    print (data)
