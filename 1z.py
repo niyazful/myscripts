38ce0d92860526ec32f3680b94f68bf35e2e0d85
niyazful <56266317+niyazful@users.noreply.github.com>
2019-10-08 12:05:25 +0500

    Delete 1z.py

--- 1z.py
+++
@@ -1,9 +0,0 @@
-import re
-import requests
-url=input("Введите URL-адрес:")'
- #\\ прямая ссылка для скачивание файла
-s=requests.get(url).text
- #\\ выделить домен с помощью регулярного выражения
-regex = re.findall("(\w*\.\w*/)", s)
-
-print (regex)
